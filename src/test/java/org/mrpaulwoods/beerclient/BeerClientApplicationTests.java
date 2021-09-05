88919bfa24e9acd166112f88e5822908893c5e26
Paul Woods <mr.paul.woods@gmail.com>
2021-09-04 19:54:45 -0500

    initial commit

---
+++ src/test/java/org/mrpaulwoods/beerclient/BeerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrpaulwoods.beerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
